9fde385964053d13bfbf094a62b4c1868e2c9ab4
pouya samie <pouya.samie@gmail.com>
2023-09-05 11:15:43 +0330

    first init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <guitarNeck msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import guitarNeck from './components/guitarNeck.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    guitarNeck,
+  },
+});
+</script>
+
+<style lang="less">
+@import url("assets/less/bootstrap.less");
+@import url("assets/less/responsive.less");
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
