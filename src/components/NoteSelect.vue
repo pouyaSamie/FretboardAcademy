3ca7b38cd851617fa90fc805e042419cefa50c3e
pouya samie <pouya.samie@gmail.com>
2023-09-17 16:26:15 +0330

    add style

--- src/components/NoteSelect.vue
+++ src/components/NoteSelect.vue
@@ -1,56 +1,76 @@
 <template>
   <div class="container">
     <button class="flex-2">Start</button>
-    <div class="flex-1">Notes</div>
+    <div class="flex-tiny"><b>Notes</b></div>
     <ul class="note-box">
-      <li class="note">A</li>
-      <li class="note">A#</li>
-      <li class="note">B</li>
-      <li class="note">C</li>
-      <li class="note">C#</li>
-      <li class="note">D</li>
-      <li class="note">D#</li>
-      <li class="note">E</li>
-      <li class="note">F</li>
-      <li class="note">F#</li>
-      <li class="note">G</li>
-      <li class="note">G#</li>
+      <li
+        v-for="note in notes"
+        :key="note"
+        class="note"
+        :class="{ selected: selectedNote === note }"
+        @click="selectNote(note)"
+        @keydown.enter="selectNote(note)"
+      >{{ note }}</li>
     </ul>
-    <div class="flex-1">Strings</div>
+    <div class="flex-1"><b>Strings</b></div>
     <div class="flex-1">Fret</div>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'NoteSelect',
-});
+const selectedNote = ref<string>('A');
+
+const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const selectNote = (note:string) => {
+  selectedNote.value = note;
+  console.log(note);
+};
 </script>
 <style lang="less">
-div .container{
+div .container {
   display: flex;
-    position: fixed;
-    justify-content: space-between;
-    width: 93%;
-    bottom: 15px;
-    background-color: rgba(110, 100, 93, 0.2);
+  position: fixed;
+  justify-content: space-between;
+  width: 93%;
+  bottom: 15px;
+  background-color: rgba(110, 100, 93, 0.2);
+  color: white;
+  height: 50px;
+  margin: 0px 4%;
+  line-height: 50px;
+}
+.note.selected {
+  background-color: rgba(14, 13, 12, 0.4);
     color: white;
-    height: 50px;
-    margin: 0px 4%;
-    line-height: 50px;
+    padding: 13px;
+    line-height: 0px;
 }
 .flex-1 {
   flex: 1;
 }
+.flex-tiny{
+  flex: 0.1;
+}
+
 .flex-2 {
-    flex: 0.3;
-    margin: 9px 30px;
+  flex: 0.3;
+  margin: 9px 30px;
 }
-.note-box{
-  border: 1px solid red;
+
+.note-box {
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  margin: 14px 10px;
 }
-.note{
-  border: 1px solid blue;
+
+.note {
+  padding: 12px;
+  line-height: 0px;
+}
+ul,
+li{
+  list-style: none
 }
 </style>
